67ed75366a01c10135a25e2b4571caa1ec9a09c8
Etko <etkomccooty16@gmail.com>
2021-05-14 13:31:09 +0100

    divs now change color when cursor hovers over them

--- js/script.js
+++ js/script.js
@@ -1,19 +1,30 @@
 // Connect container div to DOM
 const container = document.getElementById('container');
 
-// Create 16 x 16 grid
-/*for (i = 0; i < 256; i++) {
-        const div = document.createElement('div');
-        div.classList.add('square');
-        container.appendChild(div);
-}*/
+// create variables for rgb values
+let red =  Math.floor(Math.random() * 255); 
+let blue = Math.floor(Math.random() * 255); 
+let green = Math.floor(Math.random() * 255); 
+console.log(red);
+console.log(blue);
+console.log(green);
 
 let createGrid = (rows, columns) => {
     for (i = 0; i < (rows * columns); i++) {
         const div = document.createElement('div');
         div.classList.add('square');
+        div.addEventListener('mouseenter', () => {
+            div.setAttribute('style', `background-color: rgb(${red}, ${green}, ${blue})`);
+        })
         container.appendChild(div);
     }   
 }
 
+let changeColor = () => {
+     
+}
+
 createGrid(16, 16);
+
+
+
