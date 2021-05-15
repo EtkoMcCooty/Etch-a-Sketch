62dc1007b7831a836252b58460ab7747e51a4c33
Etko <etkomccooty16@gmail.com>
2021-05-15 11:38:27 +0100

    container grid now changes dynamically to fit the grid created with the createGrid() method

--- js/script.js
+++ js/script.js
@@ -1,8 +1,14 @@
 // Connect container div to DOM
 const container = document.getElementById('container'); 
 
+// Button that creates a new grid 
+/*const button = document.createElement('button');
+button.textContent = 'New Grid';
+container.append(button);*/
+
 let createGrid = (rows, columns) => {
     for (i = 0; i < (rows * columns); i++) {
+        container.setAttribute('style', `grid-template-columns: repeat(${columns}, 1fr); grid-template-rows: repeat(${rows}, 50px)`);
         const div = document.createElement('div');
         div.classList.add('square');
         div.addEventListener('mouseenter', () => {
@@ -21,5 +27,11 @@ let randomColor = () => {
 
 createGrid(16, 16);
 
+// Function to clear the current grid
+let clearGrid = (rows, columns) => {
+    for (i = 0; i < (rows * columns); i++) {
+        container.remove(div);
+    }
+}
 
 
